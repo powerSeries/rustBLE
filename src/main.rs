fbc5a7a6117396c47e785824c8478b70c9f81280
powerSeries <joseesotolongo@gmail.com>
2024-09-15 14:42:26 -0400

    Blink way #1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
+use core::ptr::write_volatile;
+
 use cortex_m::asm::nop;
+use cortex_m_rt::entry;
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
 
 
 #[entry]
 fn main() -> ! {
-    let mut x: usize = 0;
+    const GPIO0_PINCNF17_ADDR: *mut u32 = 0x5000_0744 as *mut u32;
+    const DIR_OUTPUT_POS: u32 = 0;
+    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
+    unsafe {
+        write_volatile(GPIO0_PINCNF17_ADDR, PINCNF_DRIVE_LED);
+    }
+    const GPIO0_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
+    const GPIO0_OUT_ROW1_POS: u32 = 17;
+    let mut is_on: bool = false;
+
     loop{
-        x += 1;
-        for _ in 0..x {
+        unsafe {
+            write_volatile(GPIO0_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
+        }
+        for _ in 0..400_000 {
             nop();
         }
+        is_on = !is_on;
     }
 }
