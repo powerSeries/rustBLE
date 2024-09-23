9f91b0df41a92788afe951bb050c4e7219a4aa20
powerSeries <joseesotolongo@gmail.com>
2024-09-22 22:44:36 -0400

    Take temperature

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,29 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use embedded_hal::digital::{OutputPin, PinState};
-use nrf52832_hal::{self as hal, gpio::Level};
+use nrf52832_hal::{self as hal, Temp};
 use hal::pac;
 
 use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 
 #[entry]
 fn main() -> ! {
+    _init_rtt();
+
     let p: pac::Peripherals = pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let mut row1  = port0.p0_17.into_push_pull_output(Level::Low);
-    let mut is_on: bool = false;
+    let mut temp: Temp = hal::temp::Temp::new(p.TEMP);
+    
     loop{
-        let _ = row1.set_state(PinState::from(is_on));
+        let deg_c: f32 = temp.measure().to_num();
+        rprintln!("Temp: {}", deg_c);
         for _ in 0..400_000 {
             nop();
         }
-        is_on = !is_on;
     }
+}
+
+fn _init_rtt() {
+    rtt_init_print!();
 }
